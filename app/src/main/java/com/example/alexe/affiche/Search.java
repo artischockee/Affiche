4e978f62541da1c77ea5ee48e2fb3f64456691ed
artyeug <artyeug@gmail.com>
2018-07-03 08:52:39 +0700

    Write some sheet in it

--- app/src/main/java/com/example/alexe/affiche/Search.java
+++ app/src/main/java/com/example/alexe/affiche/Search.java
@@ -1,17 +1,22 @@
 package com.example.alexe.affiche;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -24,6 +29,9 @@ public class Search extends Fragment {
     static View.OnClickListener myOnClickListener;
     private static ArrayList<Integer> removedItems;
 
+    private DBHelper dbhelper;
+    private SQLiteDatabase sqLiteDatabase;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -31,7 +39,16 @@ public class Search extends Fragment {
 
         myOnClickListener = new MyOnClickListener(getActivity());
 
+        Context context = getActivity();
+        dbhelper = new DBHelper(context);
+        sqLiteDatabase = dbhelper.getReadableDatabase();
+
         return view;
+    }
+
+    private void searchItem(String itemQuery) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
     }
 
     private class MyOnClickListener implements View.OnClickListener {
