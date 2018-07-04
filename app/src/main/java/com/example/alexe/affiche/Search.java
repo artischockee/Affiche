39d716792261126268130ca6c48784d13ad90d67
artyeug <artyeug@gmail.com>
2018-07-04 17:05:04 +0700

    Add improved version of the module

--- app/src/main/java/com/example/alexe/affiche/Search.java
+++ app/src/main/java/com/example/alexe/affiche/Search.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,24 +19,19 @@ import android.widget.ImageView;
 import java.util.ArrayList;
 
 public class Search extends Fragment {
-    private static RecyclerView.Adapter adapter01;
-    private static RecyclerView.Adapter adapter02;
-    private static RecyclerView.Adapter adapter03;
-    private static RecyclerView.Adapter adapter04;
+    private static AdapterUniversal adapter;
+    private static ArrayList<ListItem> arrayList = new ArrayList<>();
     public RecyclerView.LayoutManager layoutManager;
     private static RecyclerView recyclerView;
-    private static ArrayList<CardModel> data01 = new ArrayList<>();
-    private static ArrayList<CardModelEvent> data02 = new ArrayList<>();
-    private static ArrayList<CardModelEvent> data03 = new ArrayList<>();
-    private static ArrayList<CardModelEvent> data04 = new ArrayList<>();
     static View.OnClickListener myOnClickListener;
-    private static ArrayList<Integer> removedItems;
+//    private static ArrayList<Integer> removedItems;
 
-    EditText et;
-    ImageView te;
+    EditText searchLine;
+    ImageView searchButton;
 
-    private DBHelper dbhelper;
     private SQLiteDatabase sqLiteDatabase;
+
+    private String searchQuery;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,25 +45,173 @@ public class Search extends Fragment {
 
         myOnClickListener = new MyOnClickListener(getActivity());
 
-        recyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+        recyclerView = view.findViewById(R.id.my_recycler_view);
         recyclerView.setHasFixedSize(true);
 
         layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        removedItems = new ArrayList<Integer>();
+//        removedItems = new ArrayList<>();
 
-        et = (EditText) view.findViewById(R.id.editText);
-
-        te = (ImageView) view.findViewById(R.id.search_button);
-        te.setOnClickListener(myOnClickListener);
+        searchLine = getActivity().findViewById(R.id.editSearch);
 
         Context context = getActivity();
-        dbhelper = new DBHelper(context);
+        DBHelper dbhelper = new DBHelper(context);
         sqLiteDatabase = dbhelper.getReadableDatabase();
 
+        EditText editText = getActivity().findViewById(R.id.editSearch);
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() != KeyEvent.ACTION_DOWN)
+                    return false;
+
+                switch (keyCode) {
+                    case KeyEvent.KEYCODE_DPAD_CENTER:
+                    case KeyEvent.KEYCODE_ENTER:
+                        performSearchOperation();
+                        return true;
+                    default:
+                        break;
+                }
+
+                return false;
+            }
+        });
+
         return view;
+    }
+
+    private ArrayList<ListItem> queryTableCinema(Cursor cursor) {
+        Log.d("Search", "queryTableCinema");
+
+        if (!cursor.moveToFirst())
+            return null;
+
+        ArrayList<ListItem> list = new ArrayList<>();
+
+        do {
+            int titleIndex = cursor.getColumnIndex(DBHelper.KEY_TITLE);
+            String databaseString = cursor.getString(titleIndex).toLowerCase();
+
+            if (!databaseString.contains(searchQuery))
+                continue;
+
+            int infoIndex = cursor.getColumnIndex(DBHelper.KEY_INFO);
+            int yearIndex = cursor.getColumnIndex(DBHelper.KEY_YEAR);
+            int durationIndex = cursor.getColumnIndex(DBHelper.KEY_DURATION);
+            int imageIndex = cursor.getColumnIndex(DBHelper.KEY_IMG);
+            int urlIndex = cursor.getColumnIndex(DBHelper.KEY_ADDRESS);
+
+            list.add(new CardModelCinema(
+                cursor.getString(titleIndex),
+                cursor.getString(infoIndex),
+                cursor.getString(imageIndex),
+                cursor.getString(yearIndex),
+                cursor.getString(durationIndex),
+                cursor.getString(urlIndex))
+            );
+        } while (cursor.moveToNext());
+
+        cursor.close();
+
+        return list;
+    }
+
+    private ArrayList<ListItem> queryTableOthers(Cursor cursor, String tableName) {
+        Log.d("Search", "queryTableOthers: " + tableName);
+
+        if (!cursor.moveToFirst())
+            return null;
+
+        ArrayList<ListItem> list = new ArrayList<>();
+
+        do {
+            int titleIndex = cursor.getColumnIndex(DBHelper.KEY_TITLE_1);
+
+            String databaseString = cursor.getString(titleIndex).toLowerCase();
+            if (!databaseString.contains(searchQuery))
+                continue;
+
+            int timeIndex = cursor.getColumnIndex(DBHelper.KEY_TIME_1);
+            int dateIndex = cursor.getColumnIndex(DBHelper.KEY_DATE_1);
+            int imageIndex = cursor.getColumnIndex(DBHelper.KEY_IMG_1);
+            int addressIndex = cursor.getColumnIndex(DBHelper.KEY_ADDRESS_1);
+            int placeIndex = cursor.getColumnIndex(DBHelper.KEY_PLACE_1);
+
+            switch (tableName) {
+                // Theater:
+                case DBHelper.TABLE_NAME_1:
+                    list.add(new CardModelTheater(
+                        cursor.getString(titleIndex),
+                        cursor.getString(timeIndex),
+                        cursor.getString(dateIndex),
+                        cursor.getString(imageIndex),
+                        cursor.getString(addressIndex),
+                        cursor.getString(placeIndex))
+                    );
+                    break;
+                // Children:
+                case DBHelper.TABLE_NAME_2:
+                    list.add(new CardModelChildren(
+                        cursor.getString(titleIndex),
+                        cursor.getString(timeIndex),
+                        cursor.getString(dateIndex),
+                        cursor.getString(imageIndex),
+                        cursor.getString(addressIndex),
+                        cursor.getString(placeIndex))
+                    );
+                    break;
+                // Events:
+                case DBHelper.TABLE_NAME_3:
+                    list.add(new CardModelEvent(
+                        cursor.getString(titleIndex),
+                        cursor.getString(timeIndex),
+                        cursor.getString(dateIndex),
+                        cursor.getString(imageIndex),
+                        cursor.getString(addressIndex),
+                        cursor.getString(placeIndex))
+                    );
+                    break;
+            }
+        } while (cursor.moveToNext());
+
+        cursor.close();
+
+        return list;
+    }
+
+    private void performSearchOperation() {
+        Log.d("Search", "performSearchOperation");
+
+        searchQuery = searchLine.getText().toString().toLowerCase();
+
+        Cursor cursor;
+
+        // Cinema:
+        cursor = sqLiteDatabase.query(DBHelper.TABLE_NAME,
+            null, null, null, null, null, null);
+        arrayList.addAll(queryTableCinema(cursor));
+
+        // Theater:
+        cursor = sqLiteDatabase.query(DBHelper.TABLE_NAME_1,
+            null, null, null, null, null, null);
+        arrayList.addAll(queryTableOthers(cursor, DBHelper.TABLE_NAME_1));
+
+        // Children:
+        cursor = sqLiteDatabase.query(DBHelper.TABLE_NAME_2,
+            null, null, null, null, null, null);
+        arrayList.addAll(queryTableOthers(cursor, DBHelper.TABLE_NAME_2));
+
+        // Events:
+        cursor = sqLiteDatabase.query(DBHelper.TABLE_NAME_3,
+            null, null, null, null, null, null);
+        arrayList.addAll(queryTableOthers(cursor, DBHelper.TABLE_NAME_3));
+
+        adapter = new AdapterUniversal(arrayList);
+        recyclerView.setAdapter(adapter);
+
+        searchQuery = null;
     }
 
     private class MyOnClickListener implements View.OnClickListener {
@@ -79,91 +223,6 @@ public class Search extends Fragment {
 
         @Override
         public void onClick(View v) {
-            selectItem(v);
-        }
-
-        private void searchTableCinema(Cursor cursor, ArrayList data, String searchQuery) {
-            Log.d("ФУНКЦИЯ", "ЗАШЛИ В ПОИСК КИНО");
-            if (!cursor.moveToFirst())
-                return;
-
-            do {
-                int titleIndex = cursor.getColumnIndex(DBHelper.KEY_TITLE);
-                String databaseString = cursor.getString(titleIndex).toLowerCase();
-                searchQuery = searchQuery.toLowerCase();
-
-                if (!databaseString.contains(searchQuery))
-                    continue;
-
-                int infoIndex = cursor.getColumnIndex(DBHelper.KEY_INFO);
-                int yearIndex = cursor.getColumnIndex(DBHelper.KEY_YEAR);
-                int durationIndex = cursor.getColumnIndex(DBHelper.KEY_DURATION);
-                int imageIndex = cursor.getColumnIndex(DBHelper.KEY_IMG);
-                int urlIndex = cursor.getColumnIndex(DBHelper.KEY_ADDRESS);
-                data.add(new CardModel(cursor.getString(titleIndex), cursor.getString(infoIndex), cursor.getString(imageIndex),
-                        cursor.getString(yearIndex), cursor.getString(durationIndex), cursor.getString(urlIndex)));
-            } while (cursor.moveToNext());
-
-            cursor.close();
-        }
-
-        private void searchTableOthers(Cursor cursor, ArrayList data, String searchQuery) {
-            Log.d("ФУНКЦИЯ", "ЗАШЛИ В ПОИСК ГОВНА");
-            if (!cursor.moveToFirst())
-                return;
-
-            do {
-                int titleIndex = cursor.getColumnIndex(DBHelper.KEY_TITLE_1);
-                String databaseString = cursor.getString(titleIndex).toLowerCase();
-                searchQuery = searchQuery.toLowerCase();
-
-                if (!databaseString.contains(searchQuery))
-                    continue;
-
-                int timeIndex = cursor.getColumnIndex(DBHelper.KEY_TIME_1);
-                int dateIndex = cursor.getColumnIndex(DBHelper.KEY_DATE_1);
-                int imageIndex = cursor.getColumnIndex(DBHelper.KEY_IMG_1);
-                int placeIndex = cursor.getColumnIndex(DBHelper.KEY_PLACE_1);
-                int addressIndex = cursor.getColumnIndex(DBHelper.KEY_ADDRESS_1);
-
-                data.add(new CardModelEvent(
-                        cursor.getString(titleIndex),
-                        cursor.getString(timeIndex),
-                        cursor.getString(placeIndex),
-                        cursor.getString(dateIndex),
-                        cursor.getString(imageIndex),
-                        cursor.getString(addressIndex)));
-
-            } while (cursor.moveToNext());
-
-            cursor.close();
-        }
-
-        private void selectItem(View v) {
-            Log.d("button", "НАЖАЛИ");
-            String searchQuery = et.getText().toString();
-
-            Cursor cursor01 = sqLiteDatabase.query(DBHelper.TABLE_NAME, null, null, null, null, null, null);
-            Cursor cursor02 = sqLiteDatabase.query(DBHelper.TABLE_NAME_1, null, null, null, null, null, null);
-            Cursor cursor03 = sqLiteDatabase.query(DBHelper.TABLE_NAME_2, null, null, null, null, null, null);
-            Cursor cursor04 = sqLiteDatabase.query(DBHelper.TABLE_NAME_3, null, null, null, null, null, null);
-
-            searchTableCinema(cursor01, data01, searchQuery);
-            searchTableOthers(cursor02, data02, searchQuery);
-            searchTableOthers(cursor03, data03, searchQuery);
-            searchTableOthers(cursor04, data04, searchQuery);
-
-            adapter01 = new CinemaAdapter(data01);
-            recyclerView.setAdapter(adapter01);
-//            adapter02 = new TheaterAdapter(data02);
-//            recyclerView.setAdapter(adapter02);
-            //adapter03 = new ChildrenAdapter(data03);
-            //recyclerView.setAdapter(adapter03);
-            //adapter04 = new EventAdapter(data04);
-            //recyclerView.setAdapter(adapter04);
-
-//            dbhelper.close();
-//            sqLiteDatabase.close();
 
         }
     }
